1f0cb73fda01bc288f48c4c2fa0d370870ad57be
sambit572 <sambitkumarbal24@gmail.com>
2025-07-24 23:31:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Listing=require("./models/listing.js");
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema,reviewSchema}=require("./schema.js");
+const Review=require("./models/review.js");
+const listings=require("./routes/listing.js");
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(()=>{
+    console.log("connected to db");
+}).catch(err=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/",(req,res)=>{
+    res.send("app has started");
+});
+
+const validateReview=(req,res,next)=>{
+let {error}=reviewSchema.validate(req.body);
+if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg);     
+} else {
+    next();
+}
+};
+
+app.use("/listings",listings);
+
+//Reviews Route
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
+    let listing=await Listing.findById(req.params.id);
+    let newReview=new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.send(`/listings/${id}`);
+}))
+
+// app.get("/testListing",async(req,res)=>{
+//     const sampleListing=new Listing({
+//         title:"my home",
+//         description:"By the Beach",
+//         price:4000,
+//         location:"Bhadrak",
+//         country:"india"
+//     })
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful");
+// })
+
+// app.all("*",(req,res,next)=>{
+//     next(new ExpressError(404,"page not found"));
+// });
+
+app.use((err,req,res,next)=>{
+    let{statusCode=500,message="something went wrong!"}=err;
+    res.status(statusCode).render("error.ejs",{message});
+    // res.status(statusCode).send(message);
+});
+
+app.listen(8080,()=>{
+    console.log("app is listening on port 8080");
+})
