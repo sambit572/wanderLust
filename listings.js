4c5c505898a6b111d61fa02ca663f628c510139a
sambit572 <sambitkumarbal24@gmail.com>
2025-07-31 21:51:32 +0530

    Add files via upload

---
+++ listings.js
@@ -0,0 +1,67 @@
+const Listing=require("../models/listing.js");
+
+module.exports.index=async(req,res)=>{
+    const allListing=await Listing.find({});
+    res.render("listings/index",{allListing});
+};
+
+module.exports.renderNewForm=(req,res)=>{
+    res.render("listings/new.ejs");
+};
+
+module.exports.showListing=async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id).populate({path:"reviews",populate:{path:"author"}})
+    .populate("owner");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist");
+        res.redirect("/listing");
+    }
+    console.log(listing);
+    res.render("listings/show.ejs",{listing});
+};
+
+module.exports.createListing=async(req,res,next)=>{
+const {title,description,image,price,location,country}=req.body;
+        const newListing=new Listing({
+        title,
+        description,
+        image:[{url:image,filename:"placeholder.jpg"}],
+        price,
+        location,
+        country
+    })
+    newListing.owner=req.user._id; 
+    await newListing.save();
+    req.flash("success","New Listing Created");
+    res.redirect("/listings");
+};
+
+module.exports.renderEditForm=async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist");
+        res.redirect("/listing");
+    }
+    res.render("listings/edit.ejs",{listing});
+};
+
+module.exports.updateListing=async(req,res,next)=>{
+    let {id}=req.params;
+    const listingData=req.body.listing;
+    if(req.body.listing.image&& Array.isArray(listingData.image)){
+        listingData.image=listingData.image;
+    }
+    await Listing.findByIdAndUpdate(id,{...listingData});
+    req.flash("success","Listing Updated");
+    res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing=async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success","Listing Deleted");
+    res.redirect("/listings");
+}
